319fae5cdbcf9e6bd45af5fa1b46f7f570c13582
Mike <mald281103@gmail.com>
2026-05-25 10:40:15 -0400

    Added files

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_s3_bucket" "lab_bucket" {
+  bucket = var.bucket_name
+
+  tags = {
+    Name = "free-tier-lab"
+  }
+}
